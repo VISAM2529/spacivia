f0ef225e95d6aac9b471e2f421827d12a4863a42
Sameer Gaikwad <sameergaikwad.delxn@gmail.com>
2025-09-14 10:19:55 +0530

    first commit

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import AppNavigator from 'components/navigation/AppNavigator';
 import './global.css';
-
+//sample
 export default function App() {
   return (
     <SafeAreaProvider>
